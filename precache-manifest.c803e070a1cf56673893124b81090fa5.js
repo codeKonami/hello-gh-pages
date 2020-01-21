3113bbccf26af2921e6e5c56f73efc92e386e49e
Thomas Foricher <thomas.foricher@gmail.com>
2020-01-21 21:45:06 +0100

    Updates

---
+++ precache-manifest.c803e070a1cf56673893124b81090fa5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "254830260459cf56167c32cef60ce15a",
+    "url": "/hello-gh-pages/index.html"
+  },
+  {
+    "revision": "975a3ae2fed79b54c849",
+    "url": "/hello-gh-pages/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "7b483e227dcdb80aba4b",
+    "url": "/hello-gh-pages/static/js/2.37ea2310.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/hello-gh-pages/static/js/2.37ea2310.chunk.js.LICENSE"
+  },
+  {
+    "revision": "975a3ae2fed79b54c849",
+    "url": "/hello-gh-pages/static/js/main.c596e9b2.chunk.js"
+  },
+  {
+    "revision": "e8f04b29438ec7fc3c25",
+    "url": "/hello-gh-pages/static/js/runtime-main.6d6ad968.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hello-gh-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
